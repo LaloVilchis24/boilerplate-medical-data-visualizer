1838a81aa0c0696d6d3d10cbc0d45754d6771656
Eduardo Vilchis Lugo <eduarvilchis123@gmail.com>
2025-08-06 21:23:36 +0000

    catplot finished

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,37 +4,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
-# 2
-df['overweight'] = None
+# 2}
+H_meters = df['height'] / 100
+BMI = df['weight'] / (H_meters ** 2)
+df['overweight'] = (BMI > 25).astype(int)
 
 # 3
+df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+df['gluc'] = (df['gluc'] > 1).astype(int)
 
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = pd.melt(df,
+                 id_vars=['cardio'],
+                 value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
 
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
     
 
     # 7
 
 
-
     # 8
-    fig = None
+    fig = sns.catplot(x='variable', y='total', hue='value', col='cardio',
+                  data=df_cat, kind='bar').fig
 
 
     # 9
     fig.savefig('catplot.png')
     return fig
 
-
+'''
 # 10
 def draw_heat_map():
     # 11
@@ -57,4 +63,4 @@ def draw_heat_map():
 
     # 16
     fig.savefig('heatmap.png')
-    return fig
+    return fig'''
